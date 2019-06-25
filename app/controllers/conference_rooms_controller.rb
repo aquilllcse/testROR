e91c1a8fd6e16ac15a258c0b7e9cd7bf06c84297
Aquilll <aquilhussain15@gmail.com>
2019-06-25 16:00:20 +0530

    add CanCan authorization

--- app/controllers/conference_rooms_controller.rb
+++ app/controllers/conference_rooms_controller.rb
@@ -1,6 +1,6 @@
 class ConferenceRoomsController < ApplicationController
     
-  before_action :find_room, only: [:update, :show, :destroy]
+  before_action :find_room, only: [:update, :show, :destroy, :current_user]
 
   def index
     @rooms = ConferenceRoom.all
@@ -16,7 +16,8 @@ class ConferenceRoomsController < ApplicationController
 
   def update
     #@room = ConferenceRoom.find(params[:id])
-    if @room.update(role_param) && authenticate_role(@room)
+    # uncomment if condition only if CanCan is not used
+    if @room.update(role_param) #&& authenticate_role(@room)
       UserMailer.with(conference_room: @room).updation.deliver
       flash[:notice] = "Room successfully edited"
       redirect_to conference_room_path(@room)
@@ -27,14 +28,18 @@ class ConferenceRoomsController < ApplicationController
   end
 
   def destroy
+    UserMailer.with(conference_room: @room).deletion.deliver
+    @room.destroy
+    redirect_to conference_rooms_path
     #@room = ConferenceRoom.find(params[:id])
-    if authenticate_role(@room)
-      UserMailer.with(conference_room: @room).deletion.deliver
-      @room.destroy
-      redirect_to conference_rooms_path
-    else
-      render plain:"Only Admin can delete"
-    end
+    # uncomment below only if not using CanCan
+    # if authenticate_role(@room)
+    #   UserMailer.with(conference_room: @room).deletion.deliver
+    #   @room.destroy
+    #   redirect_to conference_rooms_path
+    # else
+    #   render plain:"Only Admin can delete"
+    # end
   end
 
   def create
@@ -50,6 +55,9 @@ class ConferenceRoomsController < ApplicationController
 
   def show ; end
 
+  def current_user
+    
+  end
   private
     def find_room
       @room = ConferenceRoom.find(params[:id])
@@ -57,12 +65,13 @@ class ConferenceRoomsController < ApplicationController
     def role_param
       params.require(:conference_room).permit(:room_number, :user_id)
     end
-
-    def authenticate_role(room)
-      if(room.user.role.name.downcase == "ADMIN".downcase)
-        return true
-      else
-        return false
-      end
-    end
+    
+    #uncomment below only if CanCan is not used
+    # def authenticate_role(room)
+    #   if(room.user.role.name.downcase == "ADMIN".downcase)
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # end
 end
